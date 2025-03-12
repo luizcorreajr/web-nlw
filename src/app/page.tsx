aeb7c62e0e0cfa0abd125aef6b1a4db7faf02232
lz-junior <correajr.dev@gmail.com>
2025-03-11 22:58:43 -0300

    iconbutton component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,18 @@
-import { ArrowRight } from 'lucide-react';
+import { ArrowRight, Copy } from 'lucide-react';
 import { Button } from "@/components/button";
+import { IconButton } from '@/components/icon-button';
 
 export default function Home() {
   return (
     <main>
       <Button>
         Enviar
-        <ArrowRight/>
+        <ArrowRight />
       </Button>
+
+      <IconButton>
+        <Copy />
+      </IconButton>
     </main>
   );
 }
