b97b912b40681da52274a429b8dc20377005ae6b
lz-junior <correajr.dev@gmail.com>
2025-03-17 23:25:31 -0300

    add export in component Input

--- src/components/input.tsx
+++ src/components/input.tsx
@@ -1,28 +1,39 @@
-import { Mail } from 'lucide-react'
 import { ComponentProps } from 'react'
 
-interface InputProps extends ComponentProps<'input'> {
+interface InputRootProps extends ComponentProps<'div'> {
   error?: boolean
 }
 
-export function Input({ error = false, ...props }: InputProps) {
+export function InputRoot({ error=false, ...props }: InputRootProps) {
   return (
     <div 
       data-error={error}
       className="group flex items-center gap-2 px-4 h-12 bg-gray-800 border border-gray-600 rounded-xl focus-within:border-gray-100 data-[error=true]:border-danger"
-    >
-      <span 
-        className="text-gray-400 group-focus-within:text-gray-100 group-[&:not(:has(input:placeholder-shown))]:text-gray-100 group-data-[error=true]:text-danger"
-      >
-        <Mail />
-      </span>
+      {...props}
+    />
+  )
+}
 
-      <div>
-        <input 
-          className="flex-1 outline-0 placeholder-gray-400"
-          {...props}
-        />
-      </div>
-    </div>
+// ======================================================
+interface InputIconProps extends ComponentProps<'span'> {}
+
+export function InputIcon(props: InputIconProps) {
+  return (
+    <span 
+      className="text-gray-400 group-focus-within:text-gray-100 group-[&:not(:has(input:placeholder-shown))]:text-gray-100 group-data-[error=true]:text-danger"
+      {...props}
+    />
+  )
+}
+
+// ======================================================
+interface InputFieldProps extends ComponentProps<'input'> {}
+
+export function InputField(props: InputFieldProps) {
+  return (
+    <input 
+      className="flex-1 outline-0 placeholder-gray-400"
+      {...props}
+    />
   )
 }
