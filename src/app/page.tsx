af84d27b494ab03516cd90d1ede311901470eb3f
lz-junior <correajr.dev@gmail.com>
2025-04-09 22:29:21 -0300

    starting...

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,9 @@
-import { ArrowRight, Copy, Mail } from 'lucide-react';
-import { Button } from "@/components/button";
-import { IconButton } from '@/components/icon-button';
-import { InputField, InputIcon, InputRoot } from '@/components/input';
+
 
 export default function Home() {
   return (
     <main>
-      <Button type="submit">
-        Enviar
-        <ArrowRight />
-      </Button>
-
-      <IconButton>
-        <Copy />
-      </IconButton>
-
-      <div>
-        <InputRoot>
-          <InputIcon>
-            <Mail className="size-5"/>
-          </InputIcon>
-
-          <InputField/>
-        </InputRoot>
-      </div>
+ 
     </main>
   );
 }
